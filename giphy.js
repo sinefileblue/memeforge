94a9db5ba5c9b96dc658b77ce8c5039ab58d2bc6
Your Name <you@example.com>
2024-05-09 12:15:41 -0400

    functions added

--- giphy.js
+++ giphy.js
@@ -2,10 +2,22 @@ const API_KEY = "9T0De2Yl0hPjBHyUWHrRuv3StHeEGlIp"
 const API_PREFIX = "https://api.giphy.com/v1/gifs/search?api_key=";
 const API_SETTINGS = "&q=&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
-function formSunmitted(event) {
+function formSubmitted(event) {
     event.preventDefault();
     let inputFieldContent = document.querySelector("[name=meme-input]").value;
     console.log(inputFieldContent);
+}
+function renderGifs(response) {
+    console.log(response);
+}
+
+function getMemes(searchExpression) {
+    fetch(
+        `${API_PREFIX}${API_KEY}&q=${searchExpression}&limit=25&${API_SETTINGS}`
+    )
+        .then((data) => data.json())
+        .then(renderGifs);
+
 }
 
 document.querySelector("#meme-form").addEventListener("submit", formSubmitted);
